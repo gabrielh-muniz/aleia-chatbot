5311ca18e7344462e6b2226babc6d5d63f435546
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-18 10:41:31 -0300

    feat: add the auth store manager with zustand and add login and logout function as well a error handler function

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,81 @@
+import { create } from "zustand";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { catchError } from "@/lib/errorHandler.js";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const defaultState = {
+  user: null,
+  isLoading: false,
+  isAuthenticated: false,
+  isCheckingAuth: false,
+  error: null,
+  isAdmin: false,
+};
+
+export const useAuthStore = create((set) => {
+  // Setup a listener for auth state changes
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      set({
+        user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } else {
+      set({
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      });
+    }
+  });
+
+  return {
+    ...defaultState,
+
+    login: async (email, password) => {
+      set({ isLoading: true, error: null });
+      const [error, userCredential] = await catchError(
+        auth.signInWithEmailAndPassword(email, password)
+      );
+      if (error) {
+        set({
+          isLoading: false,
+          error: error.message,
+        });
+        return;
+      }
+      set({
+        user: userCredential.user,
+        isAuthenticated: true,
+        isLoading: false,
+        error: null,
+      });
+    },
+
+    logout: async () => {
+      set({ isLoading: true, error: null });
+      const [error, _] = await catchError(auth.signOut());
+      if (error) {
+        set({ isLoading: false, error: error.message });
+        return;
+      }
+      set({
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+        error: null,
+      });
+    },
+  };
+});
