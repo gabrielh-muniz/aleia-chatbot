a267e8111e636efbb7aa54fcce797fc35f94d747
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-19 09:17:40 -0300

    feat: add a auth control component to redirect if the user is authenticated to dashboard or render the children if not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import ForgotPasswordPage from "@/pages/ForgotPasswordPage";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import UserAuthControl from "@/components/UserAuthControl";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<div>Hello, App!</div>} />
-        <Route path="/auth/login" element={<LoginPage />} />
-        <Route path="/auth/forgot-password" element={<ForgotPasswordPage />} />
+        <Route
+          path="/auth/login"
+          element={
+            <UserAuthControl>
+              <LoginPage />
+            </UserAuthControl>
+          }
+        />
+        <Route
+          path="/auth/forgot-password"
+          element={
+            <UserAuthControl>
+              <ForgotPasswordPage />
+            </UserAuthControl>
+          }
+        />
         <Route
           path="/dashboard"
           element={
--- frontend/src/components/UserAuthControl.jsx
+++ frontend/src/components/UserAuthControl.jsx
@@ -1,10 +1,10 @@
 import { useAuthStore } from "@/store/auth";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
+export default function UserAuthControl({ children }) {
   const { isAuthenticated, isCheckingAuth } = useAuthStore();
 
   if (isCheckingAuth) return <div>Loading...</div>;
 
-  return !isAuthenticated ? <Navigate to="/auth/login" replace /> : children;
+  return isAuthenticated ? <Navigate to="/dashboard" replace /> : children;
 }
