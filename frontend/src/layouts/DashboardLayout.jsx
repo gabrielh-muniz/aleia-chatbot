7e3a89a87d05b57bce6c23f8da28d8ee72005675
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-22 12:56:03 -0300

    style: add the sidebar, sidebar trigger and the header

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,6 +1,5 @@
 import AppSidebar from "@/components/AppSidebar";
-import { useAuthStore } from "@/store/auth";
-import { useNavigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarProvider,
@@ -9,19 +8,10 @@ import {
 } from "@/components/ui/sidebar";
 
 function DashboardLayout() {
-  const { logout } = useAuthStore();
-
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await logout();
-    navigate("/auth/login");
-  };
-
   return (
     <SidebarProvider className="flex h-screen">
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="">
         <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
@@ -29,12 +19,6 @@ function DashboardLayout() {
         <main>
           <Outlet />
         </main>
-        <button
-          className="absolute bottom-4 right-4 bg-red-500 text-white px-4 py-2 rounded"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
       </SidebarInset>
     </SidebarProvider>
   );
