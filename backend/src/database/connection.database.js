bfede0800885449fb388ad84aeaba8af48e32ac5
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-08-11 09:29:50 -0300

    feat: add the database connection and a helper function to handle errors in a go-like style

---
+++ backend/src/database/connection.database.js
@@ -0,0 +1,31 @@
+import { config } from "dotenv";
+import { Pool } from "pg";
+
+config();
+
+const dbConfig = {
+  connectionString: process.env.DB_URL,
+  max: parseInt(process.env.DB_MAX_CLIENTS || "10"),
+  idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT || "30000"),
+  connectionTimeoutMillis: parseInt(
+    process.env.DB_CONNECTION_TIMEOUT || "5000"
+  ),
+};
+
+const pool = new Pool(dbConfig);
+
+pool.on("connect", () => {
+  console.log("Database connection established");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+// Basic query function
+function query(text, params) {
+  return pool.query(text, params);
+}
+
+export { query };
